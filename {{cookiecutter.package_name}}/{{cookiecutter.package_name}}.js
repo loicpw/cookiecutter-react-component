121c932d9c91bf4e90835ef191eda9d083a759d9
Loïc Peron <peronloic.us@gmail.com>
2019-05-05 13:13:47 -0600

    use JSDoc style for comments and add fields into package.json

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}.js
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}.js
@@ -1,77 +1,105 @@
-/* ---------------------------------------------------------------------
- === "{{cookiecutter.component_name}}" ===
-
- author:       {{cookiecutter.author_name}}
- email:        {{cookiecutter.author_email}}
- github:       {{cookiecutter.author_github}}
-
- component:    {{cookiecutter.component_name}}
- description:  {{cookiecutter.component_description}}
-----------------------------------------------------------------------*/
+/**
+ * {{cookiecutter.component_name}}
+ *
+ * component:    {{cookiecutter.component_name}}
+ * description:  {{cookiecutter.component_description}}
+ *
+ * @exports {{cookiecutter.component_name}}
+ */
 import React, { Component } from 'react';
 import './{{cookiecutter.package_name}}.css';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import theme from 'styled-theming';
 
+// XXX
+// ---------------------------------------------------------------------
+// if using react-store:
+// ---------------------------------------------------------------------
+// import { withStore } from '@spyna/react-store';
+// export default withStore(MyComponent);
+// usage inside the component:
+//  const store = this.props.store;
+//  const someState = store.get('key', default);
+//  store.set('key', value);
+// ---------------------------------------------------------------------
 
-/* ---------------------------------------------------------------------
- — example: background color theme —
- 
- uses 'styled-theming' and 'styled-components'.
-----------------------------------------------------------------------*/
-/*
-const backgroundColor = theme.variants('mode', 'variant', {
-    default: {
-        default: props => props.theme.grey,
-        //light: props => props.theme.,
-        //dark: props => props.theme.
-    },
-    primary: {
-        default: props => props.theme.primary,
-        //light: props => props.theme.,
-        //dark: props => props.theme.
-    },
-    success: {
-        default: props => props.theme.green,
-        //light: props => props.theme.,
-        //dark: props => props.theme.
-    },
-    warning: {
-        default: props => props.theme.orange,
-        //light: props => props.theme.,
-        //dark: props => props.theme.
-    }
-});
-
-
-const Button = styled.button`
-  background-color: ${backgroundColor};
-`;
-
-Button.propTypes = {
-  variant: PropTypes.oneOf(['default', 'primary', 'success', 'warning'])
-};
-
-Button.defaultProps = {
-  variant: 'default',
-};
-
-
-// usage:
-//        <Button>default</Button>
-//        <Button variant="primary">primary</Button>
-//        <Button variant="success">success</Button>
-//        <Button variant="warning">warning</Button>
-*/
+// XXX
+// ---------------------------------------------------------------------
+// if using theme:
+// ---------------------------------------------------------------------
+// =>> if mode only
+//const BG_COLOR = theme('mode', {                                               
+//    default: props => props.theme.colors.primary,
+//    light: props => props.theme.colors.primary,
+//});
+//
+// =>> if mode and variant
+//const BG_COLOR = theme.variants('mode', 'variant', {
+//    default: {
+//        default: props => props.theme.colors.primary,
+//        light: props => props.theme.colors.primary,
+//    },
+//    success: {
+//        default: props => props.theme.colors.primary,
+//        light: props => props.theme.colors.primary,
+//    },
+//    warning: {
+//        default: props => props.theme.colors.primary,
+//        light: props => props.theme.colors.primary,
+//    }
+//});
+//Button.propTypes = {
+//  variant: PropTypes.oneOf(['default', 'success', 'warning'])
+//};
+//Button.defaultProps = {
+//  variant: 'default',
+//};
+//<Button>default button</Button>
+//<Button variant="success">success button</Button>
+// ---------------------------------------------------------------------
 
 
-/* ---------------------------------------------------------------------
- — "{{cookiecutter.component_name}}" —
- 
- TODO
-----------------------------------------------------------------------*/
+/**
+ * {{cookiecutter.component_name}}
+ *  
+ * @todo not implemented
+ *
+ * +--------------------------------+
+ * |            child 1             |
+ * +--------------------------------+
+ * |            child 2             |
+ * +--------------------------------+
+ * |            ...                 |
+ * +--------------------------------+
+ * |            child N             |
+ * +--------------------------------+
+ * 
+ * @param {type}   var           Description.
+ * @param {type}   [var]         Description of optional variable.
+ * @param {type}   [var=default] Description of optional variable with default variable.
+ * @param {Object} objectVar     Description.
+ * @param {type}   objectVar.key Description of a key in the objectVar parameter.
+ *
+ * @example
+ * <body>
+ *   <{{cookiecutter.component_name}}>
+ *     <p>child 1</p>
+ *     <p>child 2</p>
+ *   </{{cookiecutter.component_name}}>
+ * </body>
+ */
 class _{{cookiecutter.component_name}} extends Component {
+    /**
+     * @constructs {{cookiecutter.component_name}}
+     */
+    constructor() {
+        super(props);
+    }
+
+    /*
+     * render {{cookiecutter.component_name}}
+     */
     render() {
         return (
             <div className={this.props.className}>
@@ -82,8 +110,30 @@ class _{{cookiecutter.component_name}} extends Component {
 }
 
 
+// prop types
+_{{cookiecutter.component_name}}.propTypes = {
+  //variant: PropTypes.oneOf(['default', 'success', 'warning'])
+};
+
+
+// default props
+_{{cookiecutter.component_name}}.defaultProps = {
+//  variant: 'default',
+};
+
+
+/**
+ * styled {{cookiecutter.component_name}} 
+ * 
+ * @todo not implemented
+ *
+ */
 const {{cookiecutter.component_name}} = styled(_{{cookiecutter.component_name}})`
     background-color: #A0A0A0;
+    /* theming:
+    background-color: ${BG_COLOR};
+    */
 `;
+
 
 export default {{cookiecutter.component_name}};
